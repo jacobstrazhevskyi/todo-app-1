c584e1ccffc0fd49474d249ae41935d99c428edb
jacob.strahzhevskyi <jacob.strahzhevskyi@gmail.com>
2024-01-24 21:49:01 +0300

    Presseting

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-<<<<<<< HEAD
-=======
-import { Test } from './Test';
-// const Test = () => ('test component');
->>>>>>> c4830f292a38ff34e7b6e9dd5c6cfc57b8b4ca12
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -13,12 +8,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
     <App />
-=======
-    {/* <App /> */}
-    <Test />
-    123
->>>>>>> c4830f292a38ff34e7b6e9dd5c6cfc57b8b4ca12
   </React.StrictMode>,
 );
