45f1e72d2468441f4730f3a5fa3958177d5b4826
jacob.strahzhevskyi <jacob.strahzhevskyi@gmail.com>
2024-02-18 23:22:12 +0300

    resolved conflits with merging

--- src/components/ThemeSwitchMenu/ThemeSwitchMenu.tsx
+++ src/components/ThemeSwitchMenu/ThemeSwitchMenu.tsx
@@ -9,13 +9,8 @@ import {
   styled,
 } from '@mui/material';
 
-<<<<<<< HEAD
 import { useAppDispatch } from '../../utils/hooks/useAppDispatch';
 // import { useDispatch } from 'react-redux';
-=======
-// import { useAppDispatch } from '../app/hooks/useAppDispatch';
-import { useDispatch } from 'react-redux';
->>>>>>> f42ad71e3d60ee5b627b6862c58b87fc0389b3a9
 import { selectTheme } from '../../redux/themeSlice';
 import { themes } from '../../themes/themes';
 
@@ -35,11 +30,7 @@ const CentredSelectMenu = styled(Select)({
 });
 
 export const ThemeSwitchMenu: React.FC = () => {
-<<<<<<< HEAD
   const dispatch = useAppDispatch();
-=======
-  const dispatch = useDispatch();
->>>>>>> f42ad71e3d60ee5b627b6862c58b87fc0389b3a9
 
   const selectThemeMenuHandler = (
     event: SelectChangeEvent<unknown>,
