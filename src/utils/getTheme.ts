714e3ad825c6eeb4f0756d6dc71cce584e47d13a
jacob.strahzhevskyi <jacob.strahzhevskyi@gmail.com>
2024-02-18 23:12:39 +0300

    renamed on getTheme

--- src/utils/getTheme.ts
+++ src/utils/getTheme.ts
@@ -4,5 +4,6 @@ import { themes } from '../themes/themes';
 export const getTheme = (targetThemeName: string): Theme => {
   const theme = themes.find(({ themeName }) => themeName === targetThemeName)
     || { themeName: '', themeObject: createTheme() };
+
   return createTheme(theme.themeObject);
 };
