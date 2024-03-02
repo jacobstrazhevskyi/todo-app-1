649baa5de276ca2746c97fa74394d4469248a054
jacob.strahzhevskyi <jacob.strahzhevskyi@gmail.com>
2024-03-02 23:08:22 +0200

    replaced all business logic in another components

--- src/redux/themeSlice.ts
+++ src/redux/themeSlice.ts
@@ -1,22 +1,11 @@
 /* eslint-disable no-param-reassign */
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
-import { localStorageKeys } from '../auxFiles/localStorageKeys';
-
-const {
-  themesKeys: {
-    OSTheme,
-    theme,
-  },
-} = localStorageKeys;
-
-const currentOSTheme = localStorage.getItem(OSTheme) || '';
-const themeNameFromLocalStorage = localStorage.getItem(theme) || '';
 
 type State = {
   name: string,
 };
 
-const initialState: State = { name: themeNameFromLocalStorage || currentOSTheme };
+const initialState: State = { name: '' };
 
 const themeSlice = createSlice({
   name: 'selectedTheme',
