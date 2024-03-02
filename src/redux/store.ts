fefc090d864c2c9d524127f4de8338a1c7334a96
jacob.strahzhevskyi <jacob.strahzhevskyi@gmail.com>
2024-03-02 23:06:46 +0200

    added todos reducer to redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import themeSlice from './themeSlice';
+import todosSlice from './todosSlice';
 
 export const store = configureStore({
   reducer: {
     theme: themeSlice,
+    todos: todosSlice,
   },
 });
 
