a8b1bb6762bae24602c38666cdeef690cb61c4a0
jacob.strahzhevskyi <jacob.strahzhevskyi@gmail.com>
2024-04-14 23:37:51 +0300

    added hover which changes opacity of secondaryAction of todo item. Also implemented todo complition logic

--- src/components/TodoItemContent/TodoItemContent.tsx
+++ src/components/TodoItemContent/TodoItemContent.tsx
@@ -5,28 +5,42 @@ import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
+  styled,
 } from '@mui/material';
+import { useAppDispatch } from '../../utils/hooks/useAppDispatch';
+import { toggleCompletedStatus } from '../../redux/todosSlice';
 
 type Props = {
   todoName: string,
   todoId: number,
+  completed: boolean,
 }
 
 export const TodoItemContent: React.FC<Props> = ({
   todoName,
   todoId,
+  completed,
 }) => {
   const checkBoxListLabel = `checkbox-list-label-${todoId}`;
+  const dispatch = useAppDispatch();
+
+  const StyledListItemText = styled(ListItemText)({
+    textDecoration: completed ? 'line-through' : '', 
+  });
 
   return (
     <ListItemButton>
       <ListItemIcon>
         <Checkbox
           inputProps={{ 'aria-labelledby': checkBoxListLabel }}
+          onChange={() => {
+            dispatch(toggleCompletedStatus(todoId));
+          }}
+          checked={completed}
         />
       </ListItemIcon>
-      <ListItemText
-        primary={todoName}
+      <StyledListItemText
+        primary={todoName}  
         id={checkBoxListLabel} 
         aria-label={checkBoxListLabel}
       />
