bcfb507adfbdbf4f6e17fc9cb67d997a5547781c
jacob.strazhevskyi <jacob.strazhevskyi@gmail.com>
2024-04-19 22:52:05 +0300

    implemented message for "no todos",  and saving todos state to localStorage

--- src/components/TodoListContent/TodoListContent.tsx
+++ src/components/TodoListContent/TodoListContent.tsx
@@ -6,7 +6,10 @@ import React, {
 import uuid from 'react-uuid';
 
 import {
+  Box,
   List,
+  Typography,
+  styled,
 } from '@mui/material';
 
 import {
@@ -18,12 +21,46 @@ import { useAppSelector } from '../../utils/hooks/useAppSelector';
 import { Todo } from '../../types/Todo';
 import { TodoItem } from '../TodoItem';
 import { CreateTodoButton } from '../CreateTodoButton';
+import useLocalStorage from '../../utils/hooks/useLocalStorage';
+import { useAppDispatch } from '../../utils/hooks/useAppDispatch';
+import { setTodos } from '../../redux/todosSlice';
+
+const StyledBox = styled(Box)({
+  padding: '0 20px',
+});
+
+const StyledTypography = styled(Typography)({
+  opacity: 0.6,
+  padding: '21px',
+});
 
 export const TodoListContent: React.FC = () => {
   const todos: Todo[] = useAppSelector(state => state.todos);
   const [todosToDisplay, setTodosToDisplay] = useState<Todo[]>(todos);
 
+  const dispatch = useAppDispatch();
+
+  const [noTodos, setNoTodos] = useState(Boolean(todos.length));
+
   const { pageNumber } = useParams();
+
+  useEffect(() => {
+    if (!todos.length) {
+      setNoTodos(false);
+    } else {
+      setNoTodos(true);
+    }
+  }, [todos]);
+
+  const [todosFromLocalStorage] = useLocalStorage('todos', todos);
+
+  useEffect(() => {
+    dispatch(
+      setTodos(todosFromLocalStorage),
+    );
+  }, []);
+
+  useEffect(() => {}, []);
 
   useEffect(() => {
     const currentPage = Number(pageNumber) || 1;
@@ -41,9 +78,22 @@ export const TodoListContent: React.FC = () => {
 
   return (
     <List>
-      {todosToDisplay.map((todo: Todo) => (
-        <TodoItem key={uuid()} {...{ todo }} />
-      ))}
+
+      {
+        noTodos ? (
+          todosToDisplay.map((todo: Todo) => (
+            <TodoItem key={uuid()} {...{ todo }} />
+          ))
+        ) : (
+          <StyledBox>
+            <StyledTypography
+              variant="h5"
+            >
+              Create new todo
+            </StyledTypography>
+          </StyledBox>
+        )
+      }
 
       <CreateTodoButton />
     </List>
