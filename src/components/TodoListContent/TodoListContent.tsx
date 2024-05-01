6d24f306f6b12d8641d88a0ce84d44e0b0658be2
jacob.strazhevskyi <jacob.strazhevskyi@gmail.com>
2024-05-01 11:02:12 +0300

    implemented a TodosSorting component, and logic of sorting and filtering todos

--- src/components/TodoListContent/TodoListContent.tsx
+++ src/components/TodoListContent/TodoListContent.tsx
@@ -14,6 +14,7 @@ import {
 
 import {
   useParams,
+  useSearchParams,
 } from 'react-router-dom';
 
 import { useAppSelector } from '../../utils/hooks/useAppSelector';
@@ -24,6 +25,25 @@ import { CreateTodoButton } from '../CreateTodoButton';
 import useLocalStorage from '../../utils/hooks/useLocalStorage';
 import { useAppDispatch } from '../../utils/hooks/useAppDispatch';
 import { setTodos } from '../../redux/todosSlice';
+
+import { setFiltredTodos } from '../../redux/filtredTodos';
+import { TodosSorting } from '../TodosSorting';
+import { getSortedTodos } from '../../utils/getSortedTodos';
+import { getFiltredTodos } from '../../utils/getFiltredTodos';
+
+import { searchParamsKeys } from '../../auxFiles/searchParamsKeys';
+import { localStorageKeys } from '../../auxFiles/localStorageKeys';
+
+const {
+  filter,
+  sort,
+} = searchParamsKeys;
+
+const {
+  todosKeys: {
+    todos: todosLocalStorageKey,
+  },
+} = localStorageKeys;
 
 const StyledBox = styled(Box)({
   padding: '0 20px',
@@ -36,23 +56,25 @@ const StyledTypography = styled(Typography)({
 
 export const TodoListContent: React.FC = () => {
   const todos: Todo[] = useAppSelector(state => state.todos);
-  const [todosToDisplay, setTodosToDisplay] = useState<Todo[]>(todos);
+  const filtredTodos = useAppSelector(state => state.filtredTodos);
 
   const dispatch = useAppDispatch();
 
+  const [todosToDisplay, setTodosToDisplay] = useState<Todo[]>(filtredTodos);
   const [noTodos, setNoTodos] = useState(Boolean(todos.length));
 
   const { pageNumber } = useParams();
+  const [searchParams] = useSearchParams();
+
+  const [todosFromLocalStorage] = useLocalStorage(todosLocalStorageKey, todos);
 
   useEffect(() => {
-    if (!todos.length) {
+    if (!filtredTodos.length) {
       setNoTodos(false);
     } else {
       setNoTodos(true);
     }
-  }, [todos]);
-
-  const [todosFromLocalStorage] = useLocalStorage('todos', todos);
+  }, [filtredTodos]);
 
   useEffect(() => {
     dispatch(
@@ -60,24 +82,45 @@ export const TodoListContent: React.FC = () => {
     );
   }, []);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const filterType = searchParams.get(filter) || '';
+
+    const filtredTodosToSet = getFiltredTodos({
+      todosToFilter: todos,
+      filterType,
+    });
+
+    dispatch(
+      setFiltredTodos(
+        filtredTodosToSet,
+      ),
+    );
+  }, [todos, searchParams]);
 
   useEffect(() => {
+    const sortType = searchParams.get(sort) || '';
+
     const currentPage = Number(pageNumber) || 1;
     const itemsPerPage = 10;
 
     const startIndex = (currentPage - 1) * itemsPerPage;
 
-    const currentItems = todos.slice(
+    const currentItems = filtredTodos.slice(
       startIndex,
       startIndex + itemsPerPage,
     );
 
-    setTodosToDisplay(currentItems);
-  }, [pageNumber, todos]);
+    const sortedTodos = getSortedTodos({
+      todosToSort: currentItems,
+      sortType,
+    });
+
+    setTodosToDisplay(sortedTodos);
+  }, [pageNumber, filtredTodos, searchParams]);
 
   return (
     <List>
+      <TodosSorting />
 
       {
         noTodos ? (
