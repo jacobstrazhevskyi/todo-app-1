40025500ee1109c86bbfd436ac4823b945922c81
jacob.strazhevskyi <jacob.strazhevskyi@gmail.com>
2024-05-01 10:54:03 +0300

    implemented an aux object for a confirming modals, dialog modals

---
+++ src/auxFiles/modalAux.ts
@@ -0,0 +1,10 @@
+export const modalAux = {
+  titles: {
+    confirmDiscardTitle: 'Are you sure you want to discard your current task?',
+    confirmDeleteTitle: 'Are you sure you want to delete current task?',
+  },
+  buttonsLables: {
+    deleteLabel: 'Delete',
+    discardLabel: 'Discard',
+  },
+};
